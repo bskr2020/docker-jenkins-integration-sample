f4b13ee241057f0e3684331f24611bc286ed5bde
bskr2020 <bskr.2020@gmail.com>
2021-05-08 20:21:57 +0530

    docker file commit.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello1..");
-        System.out.println("Hello2..");
+        System.out.println("Hello..");
+        System.out.println("Hai..");
+        System.out.println("How are you..");
+        System.out.println("Welcome to jenkins and docker in devops..");
     }
 }
